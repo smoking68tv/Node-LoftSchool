cf2d780d72a499a80c4e6597c547fb73b6dd4033
Alexey <100.1000@list.ru>
2019-09-25 23:06:43 +0500

    изменил на колбэки, нет удаления папок

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const fs = require('fs');
+
 const fs = require('fs-extra');
 const path = require('path');
 
@@ -8,77 +8,72 @@ const base = process.argv[3];
 const deleteFolder = process.argv[4] && process.argv[4] === 'true' ? true : false;
 
 const readDir = (base, level) => {
-    const files = fs.readdirSync(base);
-    
-    if(!fs.existsSync(baseCollection)) {
-        fs.mkdirSync(baseCollection); 
-    }
-
-    let objFiles = [];
-    files.forEach(item => {
-        let localBase = path.join(base, item),
-            state = fs.statSync(localBase);
-
-        if (state.isDirectory()) {
-            readDir(localBase, level + 1);
+    fs.access(baseCollection, err => {
+        if(err) {
+            fs.mkdir(baseCollection, (err) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                } else {
+                    readFolders(base, level);
+                }
+            })
         } else {
-            objFiles.push({
-                base:localBase,
-                name:item
-            });
+            readFolders(base, level);
         }
     });
-    objFiles.length ? readCollection(objFiles) : {};
 };
 
-const readCollection = (files) => {
-    let folderName,
-        localBaseCollection,
-        newPath;
-
-    for(let file of files) {
-        folderName = getFirstLetter(file.name);
-        localBaseCollection = path.join(baseCollection, folderName);
-        newPath = path.join(localBaseCollection, file.name);
-
-        if(!fs.existsSync(localBaseCollection)) {
-            fs.mkdirSync(localBaseCollection);
+const readFolders = (base, level) => {
+    fs.readdir(base, (err, files) => {
+        if(err) {
+            console.log(err);
+            return;
         }
-        copyFile(file.base, file.name, newPath); 
-    }
+        files.forEach(item => {
+            let localBase = path.join(base, item);
+            fs.stat(localBase, (err, stats) => {
+                if (!stats.isFile()) {
+                    readDir(localBase, level + 1);
+                } else {
+                    readCollection({base: localBase, name: item});
+                }
+            });      
+        });
+    });
 };
+
+const readCollection = ({base, name}) => {
+    let folderName = name.slice()[0].toUpperCase(),
+        localBaseCollection = path.join(baseCollection, folderName),
+        newPath = path.join(localBaseCollection, name);
+    
+    fs.access(localBaseCollection, err => {
+        if(err) {
+            fs.mkdir(localBaseCollection, err => {
+                copyFile(base, name, newPath);
+            });
+        } else {
+            copyFile(base, name, newPath);
+        }
+    });
+};
+
 const copyFile = (base, name, newPath) => {
     fs.link(base, newPath, err => {
         if(deleteFolder) {
-        //     fs.remove(base.replace(name, ''), err => {
-        //         console.error(err)
-        //     });
             fs.unlink(base, err => {
-                // if(fs.existsSync(file.base.replace(file.name, ''))) {
-                //     fs.rmdir(file.base.replace(file.name, ''), err => {
-                //         if(err) {
-                //             console.log(err)
-                //             return;
-                //         }
-                //     })
-                // } 
                 if(err) {
                     console.log(err);
                     return;
                 }
             });
         }
-        
         if(err) {
             console.log(`${name} уже в коллекции`);
             return;
         }
     });
-};
-
-const getFirstLetter = file => {
-    let letter = file.slice()[0];
-    return letter.toUpperCase();
 };
   
 readDir(base, 0);
